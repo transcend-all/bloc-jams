c55b223c90bf601e2c51138fcaef2b4ab6666eda
Evan Flint <transcendentalguy@gmail.com>
2016-11-04 17:54:46 -0400

    9 DOM Scripting: Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-            var animatePoints = function(){
-        
-            var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point'); 
+
+var animatePoints = function(points){
                 
             var revealPoint = function(){
                 for (var i=0; i < points.length; i++){
@@ -9,9 +9,25 @@
                     points[i].style.msTransform = "scaleX(1) translateY(0)";
                     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
                 }
-            };
-                
-            revealPoint(points);
-            
-            
         };
+                
+     revealPoint(points);
+            
+};
+
+window.onload = function(){
+    
+    if(window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+       if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+           animatePoints(pointsArray);
+       }
+    });
+}
