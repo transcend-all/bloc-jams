cf59d162f0e96398c4bd90f50f272091bad07a8b
Evan Flint <transcendentalguy@gmail.com>
2016-11-18 13:02:39 -0500

    Assignment 13 Compelted

--- scripts/album.js
+++ scripts/album.js
@@ -79,6 +79,15 @@ var setCurrentAlbum = function(album){
 var findParentByClassName = function(element, targetClass){
     if (element){
         var currentParent = element.parentElement;
+        
+        if(currentParent == null || currentParent == undefined ){
+            alert("No Parent Found");
+        }
+        
+        if(currentParent.className == null || currentParent.className == undefined){
+            alert("No parent found with that class name");
+        }
+        
         while (currentParent.className != targetClass && currentParent.className !== null){
             currentParent = currentParent.parentElement;
         }
