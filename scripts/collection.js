99fa0413761c1ed7dcc5227e43053d2cde14afca
Evan Flint <transcendentalguy@gmail.com>
2016-11-20 20:53:16 -0500

    Completed Checkpoint 17

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate = 
+var buildCollectionItemTemplate = function(){
+    var template  = 
     '<div class="collection-album-container column fourth">'
     + ' <img src="assets/images/album_covers/01.png"/>'
     + ' <div class="collection-album-info caption">'
@@ -13,14 +14,17 @@ var collectionItemTemplate =
     + ' </div>'
     + ' </div>'
     ;
+    
+    return $(template);
+};
 
-window.onload = function(){
-    //#1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    //#2
-    collectionContainer.innerHTML = "";
-    //#3
+$(window).load(function(){
+   var $collectionContainer = $('.album-covers');
+   
+    $collectionContainer.empty();
+    
     for (var i=0;i<12;i++){
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
